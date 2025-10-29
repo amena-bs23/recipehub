35a6148e7cc1649519f8b78fb5e831c403bb091e
amena-bs23 <amena.khatun@brainstation-23.com>
2025-10-29 20:14:59 +0600

    feat: Add favorite route

--- lib/src/presentation/core/router/router.dart
+++ lib/src/presentation/core/router/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/favorites/view/favorites_page.dart';
 import '../../features/recipe_list/view/recipe_list_page.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
@@ -8,6 +9,9 @@ final appRouterProvider = Provider<GoRouter>((ref) {
     initialLocation: '/home',
     routes: [
       GoRoute(path: '/home', builder: (_, __) => const RecipeListPage()),
+      GoRoute(path: '/favorites', builder: (_, __) => const FavoritesPage()),
+      // GoRoute(path: '/login', builder: (_, __) => const LoginPage()), // Add if not exists
+
       // add /favorites, /profile, /recipe/:id etc later
     ],
   );
